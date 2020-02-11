482bc1a53a28c86a40dd73c3e5aa79501bf9c88d
Lucas Ketchie <lucasketchie@ORRHS.ORG>
2020-02-11 08:56:04 -0500

    Moved folders

---
+++ Clicker Game/app.js
@@ -0,0 +1,17 @@
+let bananaImg
+let heading()
+function setup(){
+createCanvas(windowWidth,indowHeight)
+bananaimg = createImg('banana.png', 'banana image')
+bananaImg.size(200,100)
+heading = createElement('h2', 'Click the Banana!')
+heading.position(width/3,height/4)
+}
+function draw(){
+bananaImg.position(random(width),random(height))
+bananaImg.mousePressed(youWon)
+}
+function youWon(){
+    heading.html('You Won!')
+    bananaImg.remove()
+}
